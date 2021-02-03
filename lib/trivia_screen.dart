f87f2ad64008aa3ff4cbf3c7a3fad6fdc5afbe38
LauraEllen17 <laura.ellen@inovafarm.com.br>
2021-02-03 13:22:04 -0300

    finalizando a tela trivia_screen

--- lib/trivia_screen.dart
+++ lib/trivia_screen.dart
@@ -8,6 +8,8 @@ class TriviaScreen extends StatefulWidget {
 
 class _TriviaScreenState extends State<TriviaScreen> {
   @override
+  int answer = 0;
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -17,6 +19,83 @@ class _TriviaScreenState extends State<TriviaScreen> {
         title: Image(
           image: AssetImage('lib/assets/icon.png'),
         ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(32, 56, 32, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'PERGUNTA 1',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'O que é flutter?',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.15),
+                          spreadRadius: 2,
+                          blurRadius: 3,
+                          offset: Offset(1, 3))
+                    ],
+                    color: Colors.white,
+                  ),
+                  child: RadioListTile(
+                    value: 1,
+                    groupValue: answer,
+                    title: Text('É um framework da Google.'),
+                    onChanged: (int value) {
+                      setState(() {
+                        answer = value;
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.15),
+                          spreadRadius: 2,
+                          blurRadius: 3,
+                          offset: Offset(1, 3))
+                    ],
+                    color: Colors.white,
+                  ),
+                  child: RadioListTile(
+                    value: 2,
+                    groupValue: answer,
+                    title: Text('É uma linguagem de programação.'),
+                    onChanged: (int value) {
+                      setState(() {
+                        answer = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
